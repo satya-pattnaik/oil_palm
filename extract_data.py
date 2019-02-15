d7c190baac43ea1084d34aea398e80891ae8d2e7
Satya <satyapattnaik76@gmail.com>
2019-02-16 01:33:02 +0530

    Extract data from zip

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,7 @@
 import zipfile
 import os
-def extract_images():
-    path_to_zip_file = os.getcwd() + '/widsdatathon2019.zip'
+def extract_images(zip_file):
+    path_to_zip_file = os.path.join(os.getcwd(),zip_file)
     zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
     zip_ref.extractall(os.getcwd())
     zip_ref.close()
